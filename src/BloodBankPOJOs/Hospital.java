a91e685d0fedbe304d120c25309720ce1eaad7fa
mariacm9802 <maria.carrascomartinez@usp.ceu.es>
2024-04-17 19:25:40 +0200

    .

--- src/BloodBankPOJOs/Hospital.java
+++ src/BloodBankPOJOs/Hospital.java
@@ -1,5 +1,65 @@
 package BloodBankPOJOs;
 
-public class Hospital {
+import java.io.Serializable;
+import java.util.Objects;
 
+public class Hospital implements Serializable {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 2983952873784901800L;
+	
+	private Integer id;
+	private String name;
+	private String address;
+	
+	public Hospital(String name, String address) {
+		super();
+		this.name = name;
+		this.address = address;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(address, id, name);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Hospital other = (Hospital) obj;
+		return Objects.equals(address, other.address) && Objects.equals(id, other.id)
+				&& Objects.equals(name, other.name);
+	}
+	
 }
--- src/BloodBankPOJOs/Stock.java
+++ src/BloodBankPOJOs/Stock.java
@@ -1,5 +1,68 @@
 package BloodBankPOJOs;
 
-public class Stock {
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
 
+public class Stock implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -6379275603644441738L;
+	
+	private Integer id;
+	private Date date;
+	private Float liters;
+	
+	public Stock(Date date, Float liters) {
+		super();
+		this.date = date;
+		this.liters = liters;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public Float getLiters() {
+		return liters;
+	}
+
+	public void setLiters(Float liters) {
+		this.liters = liters;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(date, id, liters);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Stock other = (Stock) obj;
+		return Objects.equals(date, other.date) && Objects.equals(id, other.id) && Objects.equals(liters, other.liters);
+	}
+	
+	
+	
+	
 }
