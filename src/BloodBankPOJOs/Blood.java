dfaa77b07db94e2ecabd63da05fa22c2742dc30e
mariacm9802 <maria.carrascomartinez@usp.ceu.es>
2024-04-17 19:29:04 +0200

    X

--- src/BloodBankPOJOs/Blood.java
+++ src/BloodBankPOJOs/Blood.java
@@ -2,4 +2,14 @@ package BloodBankPOJOs;
 
 public class Blood {
 
+	/*public enum TipoSangre {
+	    A_POSITIVO,
+	    A_NEGATIVO,
+	    B_POSITIVO,
+	    B_NEGATIVO,
+	    AB_POSITIVO,
+	    AB_NEGATIVO,
+	    O_POSITIVO,
+	    O_NEGATIVO;
+	}*/
 }
