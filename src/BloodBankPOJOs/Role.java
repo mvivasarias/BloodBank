847bf3e486b0f19bcef37f63b188ac29b5fa6acd
mariacm9802 <maria.carrascomartinez@usp.ceu.es>
2024-04-25 19:44:51 +0200

    creating user and role

---
+++ src/BloodBankPOJOs/Role.java
@@ -0,0 +1,59 @@
+package BloodBankPOJOs;
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.Objects;
+
+public class Role implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7778496238707742044L;
+	
+	private Integer id;
+	private String name;
+	private List<User> users;
+	public Role() {
+		super();
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public List<User> getUsers() {
+		return users;
+	}
+	public void setUsers(List<User> users) {
+		this.users = users;
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, name, users);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Role other = (Role) obj;
+		return Objects.equals(id, other.id) && Objects.equals(name, other.name) && Objects.equals(users, other.users);
+	}
+	@Override
+	public String toString() {
+		return "Role [id=" + id + ", name=" + name + "]";
+	}
+
+	
+}
