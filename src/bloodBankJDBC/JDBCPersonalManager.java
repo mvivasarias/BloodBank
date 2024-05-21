a40f3ef5f5b79b2a553b1eb8b56359d10aa82f82
mvivasarias <m.vivas5@usp.ceu.es>
2024-05-21 17:46:30 +0200

    -

--- src/bloodBankJDBC/JDBCPersonalManager.java
+++ src/bloodBankJDBC/JDBCPersonalManager.java
@@ -74,7 +74,7 @@ public class JDBCPersonalManager implements PersonalManager {
 
 				System.out.println("You are successfuly registered as personal in the blood bank database with email: "+emailSearch+"contiue with your action ");
 			} else {
-				System.out.println("You are not in the blood bank database, please register first!");
+				System.out.println("You are not in the blood bank database, please register first OPTION 1 in Personal Menu!");
 			}
 			rs.close();
 			prep.close();
