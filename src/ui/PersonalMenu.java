69d259caa118332335ab27748769b62cebcf9a24
mvivasarias <m.vivas5@usp.ceu.es>
2024-05-23 11:02:07 +0200

    -

--- src/ui/PersonalMenu.java
+++ src/ui/PersonalMenu.java
@@ -244,10 +244,11 @@ public class PersonalMenu {
 				System.out.println("Donor´s surname modification :");
 				String surname = Utilities.readString();
 
-				System.out.println("Donor´s dob ");
-				System.out.println("Donor´s dob d");
+				System.out.println("Donor´s dob ");;
 				LocalDate dob= Utilities.getDateFromKeyboard();
 				Date dobSql=Date.valueOf(dob);
+				
+				Date dobSQL=date(dobSql/1000, 'unixepoch','localtime');
 
 				String bloodType = Utilities.askBloodType("Donor´s blood type modification");
 
